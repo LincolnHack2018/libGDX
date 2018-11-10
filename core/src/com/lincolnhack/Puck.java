df423641498bb940b33637ed6eb44b04361a41e6
Calvin-J-Scottt <calvinjohnscott@gmail.com>
2018-11-10 15:17:56 +0000

    creating puck to be dynamic

--- core/src/com/lincolnhack/Puck.java
+++ core/src/com/lincolnhack/Puck.java
@@ -2,10 +2,13 @@ package com.lincolnhack;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
@@ -15,21 +18,32 @@ public class Puck extends Image {
 
     public Puck(TextureRegion textureRegion, World world, float x, float y, float radius, float angle) {
         super(textureRegion);
+
         this.setSize(radius, radius);
         this.setOrigin(this.getWidth()/2,this.getHeight()/2);
         this.rotateBy(angle);
         this.setPosition(x,y);
         this.world = world;
-        BodyDef groundBodyDef = new BodyDef();
+        BodyDef puckBodyDef = new BodyDef();
+        puckBodyDef.type = BodyDef.BodyType.DynamicBody;
 
-        groundBodyDef.position.set(new Vector2(x, y));
-        body = world.createBody(groundBodyDef);
+        puckBodyDef.position.set(new Vector2(x, y));
+
+        // If we always want to apply force at the center of the body, use the following
+        body = world.createBody(puckBodyDef);
 
         CircleShape circleShape = new CircleShape();
         circleShape.setRadius(this.getWidth()/2);
         body.setTransform(this.getX()+this.getWidth()/2,this.getY()+this.getHeight()/2, (float)Math.toRadians(angle));
 
-        body.createFixture(circleShape, 0.0f);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = circleShape;
+        fixtureDef.density = 5f;
+        fixtureDef.friction = 0f;
+        fixtureDef.restitution= 1f;
+        Fixture fixture = body.createFixture(fixtureDef);
+
 
         circleShape.dispose();
     }
@@ -38,6 +52,12 @@ public class Puck extends Image {
     public void act(float delta) {
         //body.setTransform(this.getX()+this.getWidth()/2,this.getY()+this.getHeight()/2, (float)Math.toRadians(getRotation()));
         super.act(delta);
+
+        this.setRotation(body.getAngle()*  MathUtils.radiansToDegrees);
+
+        this.setPosition(body.getPosition().x-this.getWidth()/2,body.getPosition().y-this.getHeight()/2);
+
+
     }
 
     @Override
